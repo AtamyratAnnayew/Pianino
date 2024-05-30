feb78cbeca2122e48ecbc7bf7743404607e31528
Atamyrat <152060732+AtamyratAnnayew@users.noreply.github.com>
2024-05-30 18:50:30 +0500

    Add files via upload

---
+++ Piano.js
@@ -0,0 +1,48 @@
+var notes = {
+	'C4': 261.6,
+	'C#4': 277.2,
+	'D4': 293.7,
+	'Eb4': 311.1,
+	'E4': 329.6,
+	'F4': 349.2,
+	'F#4': 370.0,
+	'G4': 392.0,
+	'G#4': 415.3,
+	'A4': 440.0,
+	'Bb4': 466.2,
+	'B4': 493.9,
+	'C5': 523.3,
+	'C#5': 554.4,
+	'D5': 587.3,
+	'Eb5': 622.3,
+	'E5': 659.3,
+	'F5': 698.5,
+	'F#5': 740.0,
+	'G5': 784.0,
+	'G#5': 830.6,
+	'A5': 880.0,
+	'Bb5': 932.3,
+	'B5': 987.8,
+	'C6': 1047.0,
+	'C#6': 1109.0
+}
+
+var keys = document.querySelectorAll(".whiteKey, .blackKey");
+for (var i = 0; i < keys.length; i++) {
+  keys[i].addEventListener("click", function() {
+    playNote();
+  });
+}
+
+function playNote(){
+	var keyPressed = event.target.getAttribute("data-note");	
+	var context = new AudioContext();
+	var o = context.createOscillator();
+	var g = context.createGain();
+	var frequency = notes[keyPressed];
+	o.frequency.value = frequency;
+	g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 2);
+	o.connect(g);
+	g.connect(context.destination);
+	o.start(0);
+};
